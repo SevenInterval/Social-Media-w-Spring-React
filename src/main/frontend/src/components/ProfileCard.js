1604167426b7028e05e3d92cb6627a01aaf6e2fb
SevenInterval <ridvanozturk94@gmail.com>
2021-01-01 16:04:17 +0300

    Display name validation when update

--- src/main/frontend/src/components/ProfileCard.js
+++ src/main/frontend/src/components/ProfileCard.js
@@ -17,6 +17,10 @@ const ProfileCard = (props) => {
     const [user, setUser] = useState({});
     const [editable, setEditable] = useState(false);
     const [newImage, setNewImage] = useState();
+    const [validationErrors, setValidationErrors] = useState({});    
+
+    const { username, displayName, image } = user;
+    const { t } = useTranslation();
 
     useEffect(() => {
         setUser(props.user);
@@ -25,24 +29,6 @@ const ProfileCard = (props) => {
     useEffect(() => {
         setEditable(pathUsername === loggedInUsername)
     }, [pathUsername, loggedInUsername])
-
-
-    const { username, displayName, image } = user;
-    const { t } = useTranslation();
-
-    const onChangeFile = (event) => {
-        if(event.target.files.length < 1) {
-            return;
-        }
-        const file = event.target.files[0];
-        const fileReader = new FileReader();
-        fileReader.onloadend = () => {
-            setNewImage(fileReader.result);
-        }
-        fileReader.readAsDataURL(file);
-    }
-
-    const pendingApiCall = useApiProgress('put', '/api/1.0/users/' + username);
 
     useEffect(() => {
         if (!inEditMode) {
@@ -53,9 +39,26 @@ const ProfileCard = (props) => {
         }
     }, [inEditMode, displayName]);
 
+    useEffect(() => {
+        setValidationErrors(previousValidationErrors =>  ({ ...previousValidationErrors, displayName: undefined }));
+    }, [updatedDisplayName])
+
+
+    const onChangeFile = (event) => {
+        if (event.target.files.length < 1) {
+            return;
+        }
+        const file = event.target.files[0];
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+            setNewImage(fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
+
     const onClickSave = async () => {
         let image;
-        if(newImage) {
+        if (newImage) {
             image = newImage.split(',')[1];
         }
         const body = { displayName: updatedDisplayName, image };
@@ -64,14 +67,18 @@ const ProfileCard = (props) => {
             setInEditMode(false);
             setUser(response.data);
         } catch (error) {
-
+            setValidationErrors(error.response.data.validationErrors);
         }
     }
+
+    const pendingApiCall = useApiProgress('put', '/api/1.0/users/' + username);
+
+    const { displayName: displayNameError } = validationErrors;
 
     return (
         <div className="card text-center">
             <div className="card-header">
-                <ProfileImageWithDefault className="rounded-circle shadow" width="200" height="200" alt={`${username} profile`} image={image} tempimage={newImage}/>
+                <ProfileImageWithDefault className="rounded-circle shadow" width="200" height="200" alt={`${username} profile`} image={image} tempimage={newImage} />
             </div>
             <div className="card-body">
                 {!inEditMode &&
@@ -86,7 +93,7 @@ const ProfileCard = (props) => {
                 }
                 {inEditMode &&
                     <div>
-                        <Input label={t("Change Display Name")} defaultValue={displayName} onChange={event => { setUpdatedDisplayName(event.target.value) }} />
+                        <Input label={t("Change Display Name")} defaultValue={displayName} onChange={event => { setUpdatedDisplayName(event.target.value) }} error={displayNameError} />
                         <input type="file" onChange={onChangeFile} />
                         <div>
                             {<ButtonWithProgress
