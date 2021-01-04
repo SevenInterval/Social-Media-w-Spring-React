bca917562ae444a8f2258b29546c47e6ce0430e4
SevenInterval <ridvanozturk94@gmail.com>
2021-01-04 20:33:10 +0300

    Hoax and user relationships

--- src/main/frontend/src/shared/ApiProgress.js
+++ src/main/frontend/src/shared/ApiProgress.js
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export const useApiProgress = (apiMethod, apiPath) => {
+export const useApiProgress = (apiMethod, apiPath, strictPath) => {
     const [pendingApiCall, setPendingApiCall] = useState(false)
 
     useEffect(() => {
         let requestInterceptor, responseInterceptor;
 
         const updateApiCallFor = (method, url, inProgress) => {
-            if (url.startsWith(apiPath) && method === apiMethod) {
+            if (method !== apiMethod) {
+                return;
+            }
+            if (strictPath && url === apiPath) {
+                setPendingApiCall(inProgress)
+            }
+            if (!strictPath && url.startsWith(apiPath)) {
                 setPendingApiCall(inProgress)
             }
         }
@@ -39,7 +45,7 @@ export const useApiProgress = (apiMethod, apiPath) => {
         return function unmount() {
             unregisterInterceptors();
         }
-    }, [apiPath, apiMethod])
+    }, [apiPath, apiMethod, strictPath])
 
     return pendingApiCall;
 }
